2782eec6439b39098c890b8f7d4486e8d38dcda6
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-07-05 14:06:32 +0200

    start

---
+++ tempData.js
@@ -0,0 +1,24 @@
+export default tempData = [
+    {
+        name: "Fest med gutta",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Slåsskamp",
+                completed: false
+            },
+            {
+                title: "Alkoholforgiftning",
+                completed: false
+            },
+            {
+                title: "Start en bibelskole",
+                completed: true
+            },
+            {
+                title: "Sove hele dagen",
+                completed: true
+            }
+        ]
+    }
+];
